e21830f27cc82c47f520f861c4b343fe2bd4564b
raihannnkahfi <108926694+raihannnkahfi@users.noreply.github.com>
2023-10-23 20:27:21 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Quick links</h3>
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>Extra links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My orders</a>
+            <a href="cart.php">My cart</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact info</h3>
+            <p> <i class="fas fa-phone"></i> +62-811-7634-7212 </p>
+            <p> <i class="fas fa-phone"></i> +62-812-3421-7632 </p>
+            <p> <i class="fas fa-envelope"></i> raihankahfi@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Malang, Indonesia - +62 </p>
+        </div>
+
+        <div class="box">
+            <h3>Follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>Raihan Kahfi</span> </div>
+
+</section>
